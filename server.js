586638e21b5cd138a66719559b16016fd63bdabb
Your Name <you@example.com>
2023-06-01 00:24:26 +0600

    module setup

--- server.js
+++ server.js
@@ -1,10 +1,46 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
+
 const app = express();
-const PORT = 8000;
+
+dotenv.config({path:'config.env'})
+const PORT = process.env.PORT || 8080;
+
+// log request 
+app.use(morgan('tiny'));
+
+//parse request to body-parse
+app.use(bodyParser.urlencoded({extended: true}));
+
+//set view engine
+app.set("view engine","ejs")
+// app.set("views",path.resolve(__dirname, "views/ejs"))
+
+//load assets
+app.use("/css",express.static(path.resolve(__dirname, "assets/css")));
+app.use("/img",express.static(path.resolve(__dirname, "assets/img")));
+app.use("/js",express.static(path.resolve(__dirname, "assets/js")));
+
+
 
 app.get("/",(req,res)=>{
     res.send("CRUD Application");
 })
+
+
+
+
+
+
+
+
+
+
+
+
 app.listen(PORT,()=>{
     console.log(`Server running on: http://localhost:${PORT}`)
 });
