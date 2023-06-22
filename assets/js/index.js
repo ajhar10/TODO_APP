81463cd9bc5d35199d1f909711c1f8e76c6c3153
Your Name <you@example.com>
2023-06-22 13:24:22 +0600

    user add in ui form

---
+++ assets/js/index.js
@@ -0,0 +1,3 @@
+$("#add_user").submit(function(event){
+    alert("Successfully Inserted!")
+})
--- server/controller/controller.js
+++ server/controller/controller.js
@@ -21,7 +21,7 @@ const user=new Userdb({
 user
     .save(user)
     .then(data=>{
-        res.send(data)
+        res.redirect("/add-user")
     })
     .catch(err=>{
     res.status(500).send({
